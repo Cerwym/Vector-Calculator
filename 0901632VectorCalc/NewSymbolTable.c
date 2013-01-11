e1eec2d64524c5e6f4032207ac476db7cc574bc1
Peter Lockett <cerwym@googlemail.com>
2013-01-11 19:18:51 +0000

    Scalar multiplication by identifier and new number, cross product multiplication and multiple identifiers in the symbol table
    Stuff above, works, happy times.

---
+++ 0901632VectorCalc/NewSymbolTable.c
@@ -0,0 +1,9 @@
+//
+//  NewSymbolTable.c
+//  0901632VectorCalc
+//
+//  Created by Peter Lockett on 11/01/2013.
+//  Copyright (c) 2013 Peter Lockett. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ 0901632VectorCalc/NewSymbolTable.h
@@ -0,0 +1,14 @@
+//
+//  NewSymbolTable.h
+//  0901632VectorCalc
+//
+//  Created by Peter Lockett on 11/01/2013.
+//  Copyright (c) 2013 Peter Lockett. All rights reserved.
+//
+
+#ifndef _901632VectorCalc_NewSymbolTable_h
+#define _901632VectorCalc_NewSymbolTable_h
+
+
+
+#endif
